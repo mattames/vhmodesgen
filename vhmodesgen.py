679c7a031538508638411fffa2f7626b6df516aa
Matt Ames <matt.ames7bc@gmail.com>
2021-05-08 15:24:18 +1000

    Fixed the algorithm so that the results match what is seen on air

--- vhmodesgen.py
+++ vhmodesgen.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
-#for i in range(1, 33326):
 i=0
 for a in range(26):
-    for b in range(26):
-        for c in range(26):
-            print(hex(i+8126464)[2:].zfill(2), "VH-" + chr(a+65) + chr(b+65) + chr(c+65))
+    for b in range(36):
+        for c in range(36):
+            if (b<26 and c<26):
+                print(hex(i+8126464)[2:].upper().zfill(2), "VH-" + chr(a+65) + chr(b+65) + chr(c+65), a, b, c)
             i += 1
 
     
 
-# 1-33325 
-# 0x7C0000 (8126464) to (0x7C822D) 8159789
+# Note the second and third characters count to 36 instead of 26...
