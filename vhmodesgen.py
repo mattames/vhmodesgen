6bcd1da9a5d83ba141762e19bb07ac7bec89b44d
Matt Ames <matt.ames7bc@gmail.com>
2021-05-08 17:00:42 +1000

    tidy up the alignment and more importantly, print out the unallocated Mode S addresses

--- vhmodesgen.py
+++ vhmodesgen.py
@@ -10,5 +10,7 @@ for a in range(26):
     for b in range(36):
         for c in range(36):
             if (b<26 and c<26):
-                print(hex(i)[2:].upper().zfill(2), "VH-" + chr(a+65) + chr(b+65) + chr(c+65), a, b, c)
+                print(hex(i)[2:].upper().zfill(2), "VH-" + chr(a+65) + chr(b+65) + chr(c+65) + "\t\t", a, b, c)
+#           else:
+#               print(hex(i)[2:].upper().zfill(2), "Unallocated\t" , a, b, c)
             i += 1
