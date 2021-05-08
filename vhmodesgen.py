96ef832ca5c76261e0e31e355ac3794bc581ddfc
Matt Ames <matt.ames7bc@gmail.com>
2021-05-08 15:46:51 +1000

    Remove magic number from print statement, start counter at the correct base address to clarify code

--- vhmodesgen.py
+++ vhmodesgen.py
@@ -5,10 +5,10 @@
 # Quick bit of Python to programmatically convert a Mode S address into an Austrailan VH- ICAO registration.
 # Note the second and third characters count to 36 instead of 26.
 
-i=0
+i=0x7C0000              # VH- Addresses begin at 0x7C0000
 for a in range(26):
     for b in range(36):
         for c in range(36):
             if (b<26 and c<26):
-                print(hex(i+8126464)[2:].upper().zfill(2), "VH-" + chr(a+65) + chr(b+65) + chr(c+65), a, b, c)
+                print(hex(i)[2:].upper().zfill(2), "VH-" + chr(a+65) + chr(b+65) + chr(c+65), a, b, c)
             i += 1
